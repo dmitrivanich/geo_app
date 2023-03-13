3dda8cad7e3dba3b2120c582098b232de079de38
dmitrivanich <dmitrivanich@gmail.com>
2023-03-13 03:10:26 +0300

    [edit] max-zoom

--- src/components/PlanComponent.jsx
+++ src/components/PlanComponent.jsx
@@ -1,18 +1,20 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Box } from "@chakra-ui/react"
+import { useEffect, useRef, useState } from 'react'
+//LEAFLET
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, GeoJSON, FeatureGroup, TileLayer } from 'react-leaflet';
 import geoData from "assets/floor"
 
 export const PlanComponent = () => {
   const position = [53.91712819154794, 27.63492423374176]  //[53.91687819154794, 27.63435423374176] - old position
+  const mapRef = useRef(null)
 
   return (
     <Flex w="100%" h="100%" >
-      <MapContainer center={position} zoom={20} style={{ width: "100%", height: "100%" }}>
-        {/* <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        /> */}
+      <MapContainer maxZoom={25} center={position} zoom={20} style={{ width: "100%", height: "100%" }}>
+        <TileLayer
+          url="https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=laR0YY8O5Cgv3pummhG5"
+        />
 
         <FeatureGroup>
           <GeoJSON data={geoData.features} />
